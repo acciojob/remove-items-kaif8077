63bbfb28869606720fa00e2ad2fae939662e3a5c
kaif8077 <149231751+kaif8077@users.noreply.github.com>
2025-11-24 21:30:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const select=document.getElementById('colorSelect')
+const btn=document.querySelector("input")
+btn.addEventListener("click", ()=>{
+	const selectedIndex = select.selectedIndex;
+	if(selectedIndex!=-1){
+		select.remove(selectedIndex)
+	}
+})
